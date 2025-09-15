7d82223c2e62908599d75c4b3253065fe35a482c
lmoralesdt <Luis.MoralesSegundo@discounttire.com>
2025-09-15 17:22:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from deployment_template import MyChart
 # Variables you want to inject
 APP_LABEL = "payment-service"
 IMAGE = "nginx:1.25.2"
-REPLICAS = 2
+REPLICAS = 3
 
 app = App()
 MyChart(app, "payment-chart", app_label=APP_LABEL, replicas=REPLICAS, image=IMAGE)
